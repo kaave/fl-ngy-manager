c143eec53c5be4d8a6594f05b095c761970456d6
kaave <abe@framelunch.jp>
2016-09-24 22:39:16 +0900

    Add radio index, create

--- app/controllers/radios_controller.rb
+++ app/controllers/radios_controller.rb
@@ -1,2 +1,39 @@
 class RadiosController < ApplicationController
+  before_action :set_radio, only: [:show]
+
+  def index
+    @radios = Radio.all
+  end
+
+  def show
+  end
+
+  def new
+    @radio = Radio.new
+  end
+
+  def create
+    @radio = Radio.new(radio_params)
+
+    respond_to do |format|
+      if @radio.save
+        format.html { redirect_to @radio, notice: 'Radio was successfully created.' }
+        format.json { render :show, status: :created, location: @radio }
+      else
+        format.html { render :new }
+        format.json { render json: @radio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def set_radio
+      @radio = Radio.find(params[:id])
+    end
+
+    def radio_params
+      params.require(:radio)
+        .permit(:name, :url, :memo)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'static_pages/index'
 
+  resources :radios, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

f32fbcdab1714e00ab52aec708393517cc4a8d11
kaave <abe@framelunch.jp>
2016-10-10 23:07:44 +0900

    Refactor: create webpack base config

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,8 +1,4 @@
-const webpack = require('webpack');
-const glob = require('glob');
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
+const { entry, output, resolve, loaders, plugins, sassLoader, postcss } = require('./webpack.config.base');
 
 /*
  * base settings
@@ -10,114 +6,17 @@ const autoprefixer = require('autoprefixer');
 const devPort = 4000;
 
 /*
- * utility methods
- */
-function getEntries(files, appendFiles = []) {
-  const result = {};
-  files.forEach(filePath => {
-    const fileName = path.basename(filePath);
-    const fileNameWithoutExt = fileName.substr(0, fileName.lastIndexOf('.'));
-    if (result[fileNameWithoutExt] == null) {
-      result[fileNameWithoutExt] = [...appendFiles];
-    }
-
-    result[fileNameWithoutExt].push(filePath);
-  });
-
-  return result;
-}
-
-/*
  * webpack settings
  */
 module.exports = {
-  entry: Object.assign({}, getEntries([
-    ...glob.sync('./frontend/javascripts/*.ts'),
-    ...glob.sync('./frontend/javascripts/*.tsx'),
-    ...glob.sync('./frontend/stylesheets/*.scss')
-  ], ['babel-polyfill']), {
-    vendor: [
-      'babel-polyfill',
-      'jquery',
-      'moment',
-      'lodash',
-      'bootstrap-sass',
-      'react',
-      'react-dom',
-      'react-redux',
-      'react-router',
-      'redux',
-      'redux-actions',
-      'redux-logger',
-      'redux-saga'
-    ],
-  }),
-  output: {
-    filename: 'javascripts/[name].js',
-    path: path.join(__dirname, 'public/dist'),
-    publicPath: `http://localhost:${devPort}/`
-  },
+  entry,
+  output: Object.assign({}, output, { publicPath: `http://localhost:${devPort}/` }),
   // devtool: 'source-map',
-  resolve: {
-    alias: {
-      actioncable: `${__dirname}/vendor/bundle/ruby/2.3.0/gems/actioncable-5.0.0.1/lib/assets/compiled/action_cable.js`
-    },
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.jsx', '.js']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loaders: ['babel', 'ts']
-      },
-      {
-        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
-      }
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new ExtractTextPlugin('stylesheets/[name].css'),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      _: 'lodash',
-      moment: 'moment'
-    }),
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'javascripts/vendor.bundle.js')
-  ],
-  sassLoader: { sourceComments: true },
-  postcss: [
-    autoprefixer({
-      browser: [
-        'last 2 versions',  // without IE, iOS, Android
-        'ie >= 9',          // IE
-        'ios >= 8',         // iOS
-        'android >= 4.4'    // Android
-      ]
-    })
-  ],
+  resolve,
+  module: { loaders },
+  plugins,
+  sassLoader,
+  postcss,
   devServer: {
     contentBase: '../public/dist',
     port: devPort
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,115 +1,24 @@
-const webpack = require('webpack');
-const glob = require('glob');
-const path = require('path');
+const { optimize: { UglifyJsPlugin } } = require('webpack');
 const saveLicense = require('uglify-save-license');
 const ManifestPlugin = require('webpack-manifest-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
 
-/*
- * utility methods
- */
-function getEntries(files, appendFiles = []) {
-  const result = {};
-  files.forEach(filePath => {
-    const fileName = path.basename(filePath);
-    const fileNameWithoutExt = fileName.substr(0, fileName.lastIndexOf('.'));
-    if (result[fileNameWithoutExt] == null) {
-      result[fileNameWithoutExt] = [...appendFiles];
-    }
-
-    result[fileNameWithoutExt].push(filePath);
-  });
-
-  return result;
-}
+const { entry, output, resolve, loaders, plugins, postcss } = require('./webpack.config.base');
 
 /*
  * webpack settings
  */
 module.exports = {
-  entry: Object.assign({}, getEntries([
-    ...glob.sync('./frontend/javascripts/*.ts'),
-    ...glob.sync('./frontend/javascripts/*.tsx'),
-    ...glob.sync('./frontend/stylesheets/*.scss')
-  ], ['babel-polyfill']), {
-    vendor: [
-      'babel-polyfill',
-      'jquery',
-      'moment',
-      'lodash',
-      'bootstrap-sass',
-      'react',
-      'react-dom',
-      'react-redux',
-      'react-router',
-      'redux',
-      'redux-actions',
-      'redux-logger',
-      'redux-saga'
-    ],
-  }),
-  output: {
-    filename: 'javascripts/[name].js',
-    path: path.join(__dirname, 'public/dist')
-  },
-  // devtool: 'source-map',
-  resolve: {
-    alias: {
-      actioncable: `${__dirname}/vendor/bundle/ruby/2.3.0/gems/actioncable-5.0.0.1/lib/assets/compiled/action_cable.js`
-    },
-    extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx', '.ts', '.tsx']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loaders: ['babel', 'ts']
-      },
-      {
-        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
-      }
-    ]
-  },
+  entry,
+  output,
+  resolve,
+  module: { loaders },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new ExtractTextPlugin('stylesheets/[name].css'),
+    ...plugins,
     new ManifestPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
+    new UglifyJsPlugin({
       minimize: true,
-      output: {
-        comments: saveLicense
-      }
+      output: { comments: saveLicense }
     })
   ],
-  postcss: [
-    autoprefixer({
-      browser: [
-        'last 2 versions',  // without IE, iOS, Android
-        'ie >= 9',          // IE
-        'ios >= 8',         // iOS
-        'android >= 4.4'    // Android
-      ]
-    })
-  ]
+  postcss
 };
